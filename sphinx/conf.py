4f47fe91188a4e9a6d9fef2866482158932dedac
Colton Garelli <Colton Garelli>
2018-06-29 15:00:49 -0400

    some problem

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '/Users/coltongarelli/SequenceAnalyzer/SequenceAnalyzer2.1/Source')
+sys.path.insert(0, os.path.abspath('../Source'))
 
 
 # -- Project information -----------------------------------------------------
