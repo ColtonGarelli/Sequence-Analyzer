4077afc18939045a7f019224d65ba1113c31f0b3
Colton Garelli <Colton Garelli>
2018-07-24 10:02:25 -0400

    readthedocs update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='SequenceAnalyzer2.1',
+    version='1.1',
+    packages=['Builder', 'Director', 'main', 'Representation', 'SecondaryBiasFinder'],
+    package_dir={'': 'Source'},
+    url='https://sequence-analyzer.readthedocs.io/en/latest/',
+    license='',
+    author='Colton Garelli',
+    author_email='coltongarelli@gmail.com',
+    description='',
+    install_requires= '',
+)
