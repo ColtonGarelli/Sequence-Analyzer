467c0488450a072da34f5090a04096ac1df9c4de
Colton Garelli <Colton Garelli>
2018-10-04 11:51:26 -0400

    trying new docs

--- Source/main.py
+++ Source/main.py
@@ -1,128 +1,19 @@
 
-
-from PyQt5 import QtWidgets
-from threading import Timer
-import SecondaryBiasFinder
+import PyQt5.QtWidgets as QtWidgets
 import Director
-import Representation
 import Builder
-from os.path import join
-import requests
 import sys
-import PyQt5.QtWidgets
-# Colton Garelli
-from Builder import UniprotBuilder
 import pprint
-import json
-import os
 from Bio.SeqRecord import SeqRecord
-import time
-
-
-def analyze_group(self, list_index):
-    group_to_analyze = self.group_list[list_index]
-    for i in range(len(group_to_analyze)):
-        seq_to_analyze = group_to_analyze.seq_bias_list[i]
-        updated = seq_to_analyze.bias_finder()
-    return True
-
-
-def __output_spreadsheet():
-    something = True
-
-    return something
-
-
-# check file format function, read in . consider making global functions
-def __input_spreadsheet():
-
-    return True
-
-
-def run_experiment(self, path_in, path_out):
-    # create seqgroup and call create groups
-    set_in = self.set_file_in_path(path_in)
-    set_out = self.set_file_out_path(path_out)
-    self.group_list = self.input_spreadsheet()
-    for i in range(len(self.group_list)):
-        post_analysis = self.analyze_group(i)
-    self.output_spreadsheet()
-    something = True
-
-    return something
-
-
-def remove_line_break(sequence):
-    new_seq = sequence.replace("\n", "")
-    print(new_seq)
-
-
-def run_database_stuff():
-    something = 0
-    as_fasta = "https://www.uniprot.org/uniprot/P12345.fasta"
-    base_url = "https://www.uniprot.org/uniprot/?query="
-    query_url = {"something": "url text", "something else": "another url"}
-    # determines what info is returned in response element
-    column_url = {"ID": "a column with useful info"}
-    format_url = "&format=fasta"
-    #
-    # example_url = "https://www.uniprot.org/uniprot/?query=insulin&" \
-    #               "sort=score&columns=id,protein names,length&format=tab"
-    #
-    another_url = "http://www.uniprot.org/uniprot/?query=arabidopsis%20thaliana&sort=score&" \
-                  "columns=id,protein names,&format=tab"
-    #
-    # ebi_url = "https://www.ebi.ac.uk/proteins/api/features?offset=0&size=100&protein=Auxin%20Response%20Factor"
-
-    this_request = requests.get(another_url)
-    print(this_request.text)
-
-    url = "https://www.uniprot.org/uniprot/?query=insulin&sort=score&columns=entry name,protein names,length&format=tab"
-    request = requests.get(url)
-    print(request.text)
-
-
-def function_for_db_fiddling():
-    none = None
-    #  sequence = ""
-    # request = requests.post("https://www.uniprot.org/uniprot/?query=reviewed:yes&random=yes")
-    # print(request.status_code)
-    # SpreadsheetIO.fasta_parser()
-    # path_in = "/Users/coltongarelli/SequenceAnalyzer/PAM/References/SEQUENCEANALYZER_Experiment1_inputfile_ACTUAL.csv"
-    # # # file_representation = Representation()
-    #
-    # director = Director()
-    # director.analysis_helper(path_in)
-    # processed = director.run_analysis()
-    # SecondaryBiasFinder.export_sec_bias_files(processed)
-    # request = requests.get()
-    # run_database_stuff()
 
 
 def function_for_experimenting():
     app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
+    window = QtWidgets.QMainWindow(flags=None)
     button = QtWidgets.QPushButton("Hello, PyQt!")
     window.setCentralWidget(button)
     window.show()
     app.exec_()
-
-
-def uniprot_test_request():
-    uniprot_builder = UniprotBuilder()
-    columns = uniprot_builder.construct_column_string(['id', 'seq'])
-    request_url = uniprot_builder.create_request_url('tardigrade', columns)
-    data = uniprot_builder.make_request_get_response(request_url)
-    record_list = uniprot_builder.uniprot_fasta_to_seqrecord(data)
-    print(record_list)
-
-
-def timer():
-
-    starttime = time.time()
-    while True:
-        print("tick")
-        time.sleep(5.0 - ((time.time() - starttime) % 5.0))
 
 
 def testing_FELLS_requesting():
